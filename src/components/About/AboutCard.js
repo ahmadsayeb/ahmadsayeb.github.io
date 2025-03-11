ba4a7dc4a9b9710bc90a50b1e14a793c05b52cc8
Ahmad Sayeb <ahmadsayeb@Ahmads-MacBook-Pro.local>
2025-03-10 23:48:12 -0400

    abbout me

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,33 +8,33 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Ahmad Sayeb </span>
+            from <span className="purple">Toronto, Canada.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I am currently employed as a Senior Data Engineer (ML Oriented) at Payfare.
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            I have a Bachelor's degree in Electrical and Electronics Engineering from Orta Doğu Technical University 
+            and a Post-graduate degree in Artificial Intelligence from Georgian College.
             <br />
             <br />
-            Apart from coding, some other activities that I love to do!
+            My professional expertise includes:
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Building ML systems including LLM solutions with fine-tuning techniques
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Architecting intelligent forecasting services and data pipelines
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <ImPointRight /> Deploying cloud infrastructure and ML workloads on Kubernetes
             </li>
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+            "Transform data into intelligence, intelligence into action!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Ahmad</footer>
         </blockquote>
       </Card.Body>
     </Card>
